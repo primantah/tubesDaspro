4a264c33e2c98216834406c443c576ca8efc8758
primantah <38172009+primantah@users.noreply.github.com>
2018-04-11 16:26:21 +0700

    Update uF17_upgradeInventori.pas

--- Daftar Program/uF17_upgradeInventori.pas
+++ Daftar Program/uF17_upgradeInventori.pas
@@ -1,42 +1,24 @@
-unit uF10_namaTemplate; //Ganti : sesuaikan dengan nama file, tapi tanpa ".pas"
+unit uF17_upgradeInventori;
 
 interface
 
 uses uP1_tipeBentukan, uP3_Umum;
 
-	procedure mainNamaTemplate(ID : integer; //hapus parameter yang tidak perlu
-									var dataBahanMentah : tabelBahanMentah; 
-									var dataBahanOlahan : tabelBahanOlahan; 
-									var dataResep : tabelResep; 
-									var dataSimulasi : tabelSimulasi); 
+	procedure mainUpgradeInventori(ID : integer; var dataSimulasi : tabelSimulasi); 
 	{ I.S : Bagaimana keadaan awal dari tiap variabel pada parameter?
 	* F.S : Bagaimana keadaan akhir dari tiap variabel pada parameter?}
 	
-	procedure contohProsedurPembantu(ID : integer; //hapus parameter yang tidak perlu
-									var dataBahanMentah : tabelBahanMentah; 
-									var dataBahanOlahan : tabelBahanOlahan; 
-									var dataResep : tabelResep; 
-									var dataSimulasi : tabelSimulasi); );
-	{ I.S : Bagaimana keadaan awal dari tiap variabel pada parameter?
-	* F.S : Bagaimana keadaan akhir dari tiap variabel pada parameter?}
-
 implementation
 
-	procedure mainNamaTemplate(ID : integer; //hapus parameter yang tidak perlu
-									var dataBahanMentah : tabelBahanMentah; 
-									var dataBahanOlahan : tabelBahanOlahan; 
-									var dataResep : tabelResep; 
-									var dataSimulasi : tabelSimulasi); 
+	procedure mainUpgradeInventori(ID : integer; var dataSimulasi : tabelSimulasi); 
 	{ I.S : Bagaimana keadaan awal dari tiap variabel pada parameter?
 	* F.S : Bagaimana keadaan akhir dari tiap variabel pada parameter?}
-	
-	procedure contohProsedurPembantu(ID : integer; //hapus parameter yang tidak perlu
-									var dataBahanMentah : tabelBahanMentah; 
-									var dataBahanOlahan : tabelBahanOlahan; 
-									var dataResep : tabelResep; 
-									var dataSimulasi : tabelSimulasi); );
-	{ I.S : Bagaimana keadaan awal dari tiap variabel pada parameter?
-	* F.S : Bagaimana keadaan akhir dari tiap variabel pada parameter?}	
+	var
+		
+	begin
+		dataSimulasi.kapasitasMaxInventori:=dataSimulasi.kapasitasMaxInventori+25;
+		dataSimulasi.jumlahDuit:=dataSimulasi.jumlahDuit-25;
+	end;
 
 end.
 
@@ -47,4 +29,4 @@ end.
 * 4. Kalo mau butuh fungsi atau prosedur topik umum kaya updateTanggal(x) dan sejenisnya, silahkan request ke gue atau bikin sendiri di uP3_umum.pas
 * 5. Silahkan perhatikan baik-baik untup tipedata bentukan di uP1_tipeBentukan.pas
 * 
-* }
+* * }
