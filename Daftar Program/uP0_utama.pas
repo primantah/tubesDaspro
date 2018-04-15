1b4c46cd0ea694d24738db635d76c8514f60af8d
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2018-04-15 08:41:38 +0700

    Update uP0_utama.pas

--- Daftar Program/uP0_utama.pas
+++ Daftar Program/uP0_utama.pas
@@ -1,7 +1,7 @@
 program TubesDaspro;
 {SPESIFIKASI : Mensimulasikan 10 hari pertama dari Engi's Kitchen}
 
-uses uP1_tipeBentukan, uP2_pesan, uP3_umum, uF1_load, uF2_exit, uF3_startSimulasi;
+uses uP1_tipeBentukan, uP2_pesan, uP3_umum, uF1_load, uF2_exit, uF3_startSimulasi, uF14_lihatResep, uF15_cariResep, uF16_tambahResep, uF17_upgradeInventori;
 
 {KAMUS UTAMA}
 var
@@ -38,8 +38,8 @@ begin
 			'lihatInventori'	: mainLihatInventori(dataBahanMentah, dataBahanOlahan, ID); //validasi waktu ID=0(belom ada simulasi)*)
 			'lihatResep'		: mainLihatResep(ID, dataResep);
 			'cariResep'			: mainCariResep(ID, dataResep);
-			(*'tambahResep'		: mainTambahResep(dataResep, ID);
-			'upgradeInventori'	: mainUpgradeInventori(dataSimulasi, ID);*)
+			'tambahResep'		: mainTambahResep(ID, dataBahanMentah, dataBahanOlahan, dataResep, dataSimulasi, dataInventoriBahanMentah);
+			'upgradeInventori'	: mainUpgradeInventori(ID, dataSimulasi);
 		else if (pos('start',perintah)>0) then 
 				if (loaded) then
 				begin
