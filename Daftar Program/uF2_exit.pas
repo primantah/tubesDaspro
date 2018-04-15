beb61133ac0642c862f021cf9b6b71b3e48a323d
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2018-04-15 22:50:40 +0700

    Update uF2_exit.pas

--- Daftar Program/uF2_exit.pas
+++ Daftar Program/uF2_exit.pas
@@ -4,15 +4,21 @@ interface
 
 uses uP1_tipeBentukan, uP2_pesan, uP3_umum;
 
-	procedure mainExit(var programSelesai: boolean; var dataSimulasi : tabelSimulasi);
+	procedure mainExit(var programSelesai: boolean;
+						dataSimulasi : tabelSimulasi;
+						dataInventoriBahanMentah : tabelBahanMentah;
+						dataInventoriBahanOlahan : tabelBahanOlahan;
+						nf_simulasi : string;
+						nf_inventoriBahanMentah : string;
+						nf_inventoriBahanOlahan : string);
 	{ Program utama dari fitur exit }
 	
-	procedure writeFile(var dataSimulasi : tabelSimulasi;
-						var dataInventoriBahanMentah : tabelBahanMentah;
-						var dataInventoriBahanOlahan : tabelBahanOlahan
-						var nf_simulasi : string;
-						var nf_inventoriBahanMentah : string
-						var nf_inventoriBahanOlahan : string);
+	procedure writeFile(dataSimulasi : tabelSimulasi;
+						dataInventoriBahanMentah : tabelBahanMentah;
+						dataInventoriBahanOlahan : tabelBahanOlahan;
+						nf_simulasi : string;
+						nf_inventoriBahanMentah : string;
+						nf_inventoriBahanOlahan : string);
 	{ Memindahkan data dari array ke file eksternal }
 	
 	procedure writeSimulasi(dataSimulasi : tabelSimulasi; nf_simulasi : string);
@@ -27,7 +33,13 @@ uses uP1_tipeBentukan, uP2_pesan, uP3_umum;
 	
 implementation
 
-	procedure mainExit(var programSelesai: boolean; var dataSimulasi : tabelSimulasi);
+	procedure mainExit(var programSelesai: boolean; 
+						dataSimulasi : tabelSimulasi;
+						dataInventoriBahanMentah : tabelBahanMentah;
+						dataInventoriBahanOlahan : tabelBahanOlahan;
+						nf_simulasi : string;
+						nf_inventoriBahanMentah : string;
+						nf_inventoriBahanOlahan : string);
 	{ Program utama dari fitur exit }
 	{ ALGORITMA}
 	begin
@@ -36,12 +48,12 @@ implementation
 		programSelesai:=true;
 	end;
 	
-	procedure writeFile(var dataSimulasi : tabelSimulasi;
-						var dataInventoriBahanMentah : tabelBahanMentah;
-						var dataInventoriBahanOlahan : tabelBahanOlahan
-						var nf_simulasi : string;
-						var nf_inventoriBahanMentah : string
-						var nf_inventoriBahanOlahan : string);
+	procedure writeFile(dataSimulasi : tabelSimulasi;
+						dataInventoriBahanMentah : tabelBahanMentah;
+						dataInventoriBahanOlahan : tabelBahanOlahan;
+						nf_simulasi : string;
+						nf_inventoriBahanMentah : string;
+						nf_inventoriBahanOlahan : string);
 	{ Memindahkan data dari array ke file eksternal }
 	{ ALGORITMA }
 	begin
@@ -61,20 +73,20 @@ implementation
 		assign(fout, nf_simulasi); rewrite(fout);
 		for i := 1 to dataSimulasi.banyakItem do 
 		begin
-			write(fout, dataSimulasi.itemKe[i].nomor); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].jumlahHariHidup); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].jumlahEnergi); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].kapasitasMaxInventori); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].totalBahanMentahDibeli); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].totalBahanOlahanDibuat); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].totalBahanOlahanDijual); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].totalResep); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].totalPemasukan); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].totalPengeluaran); write(fin, ' | ');
-			write(fout, dataSimulasi.itemKe[i].totalPendapatan); write(fin, ' | ');
+			write(fout, dataSimulasi.itemKe[i].nomor); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].jumlahHariHidup); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].jumlahEnergi); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].kapasitasMaxInventori); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].totalBahanMentahDibeli); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].totalBahanOlahanDibuat); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].totalBahanOlahanDijual); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].totalResepDijual); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].totalPemasukan); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].totalPengeluaran); write(fout, ' | ');
+			write(fout, dataSimulasi.itemKe[i].totalPendapatan); write(fout, ' | ');
 			write(fout, dataSimulasi.itemKe[i].jumlahDuit); writeln(fout,'');
 		end;
-		close(fout):
+		close(fout);
 	end;
 	
 	procedure writeInventoriBahanMentah(dataInventoriBahanMentah : tabelBahanMentah; nf_inventoriBahanMentah : string);
@@ -87,9 +99,9 @@ implementation
 		assign(fout, nf_inventoriBahanMentah); rewrite(fout);
 		for i := 1 to dataInventoriBahanMentah.banyakItem do 
 		begin
-			write(fout, dataInventoriBahanMentah.itemKe[i].nomor); write(fin, ' | ');
-			write(fout, dataInventoriBahanMentah.itemKe[i].tanggalBeli); write(fin, ' | ');
-			write(fout, dataInventoriBahanMentah.itemKe[i].jumlahTersedia); writeln(fin,'');
+			write(fout, dataInventoriBahanMentah.itemKe[i].nama); write(fout, ' | ');
+			//write(fout, tulisTanggalKeFile(dataInventoriBahanMentah.itemKe[i].tanggalBeli); write(fout, ' | ');
+			write(fout, dataInventoriBahanMentah.itemKe[i].jumlahTersedia); writeln(fout,'');
 		end;
 		close(fout);
 	end;
@@ -102,11 +114,11 @@ implementation
 	{ ALGORITMA LOKAL }
 	begin
 		assign(fout, nf_inventoriBahanOlahan); rewrite(fout);
-		for i := 1 to dataInventoriBahanMentah.banyakItem do 
+		for i := 1 to dataInventoriBahanOlahan.banyakItem do 
 		begin
-			write(fout, dataInventoriBahanOlahan.itemKe[i].nomor); write(fin, ' | ');
-			write(fout, dataInventoriBahanOlahan.itemKe[i].tanggalBeli); write(fin, ' | ');
-			write(fout, dataInventoriBahanOlahan.itemKe[i].jumlahTersedia); writeln(fin,'');
+			write(fout, dataInventoriBahanOlahan.itemKe[i].nama); write(fout, ' | ');
+			//write(fout, tulisTanggal(dataInventoriBahanOlahan.itemKe[i].tanggalJual)); write(fout, ' | ');
+			write(fout, dataInventoriBahanOlahan.itemKe[i].jumlahTersedia); writeln(fout,'');
 		end;
 		close(fout);
 	end;	
