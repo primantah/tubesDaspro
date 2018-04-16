a66d7bec6738ab5d8b154a253660fdadc29298f8
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2018-04-16 21:04:47 +0700

    Update uF5_beliBahan.pas

--- Daftar Program/uF5_beliBahan.pas
+++ Daftar Program/uF5_beliBahan.pas
@@ -7,6 +7,8 @@ uses uP1_tipeBentukan, uP3_Umum;
 	procedure mainBeliBahan(ID : integer; dataBahanMentah: tabelBahanMentah;var dataSimulasi: tabelSimulasi;//hapus parameter yang tidak perlu
 									var inventoriBahanMentah : tabelBahanMentah );
 	{ Menjalankan fungsi beliBahan }
+	function cariKadaluarsa(bahanYangDibeli : string; dataBahanMentah : tabelBahanMentah):integer;
+	{Mencari index bahanYangDibeli pada array dataBahanMentah}
 	
 
 implementation
@@ -49,13 +51,20 @@ implementation
 				writeln('Total harga: ', dataBahanMentah.itemKe[i].hargaBeli*kuantitas);
 				writeln('Pembelian Sukses');
 				index:= inventoriBahanMentah.banyakItem+1; 
+				
 				{memasukan ke array inventoriBahanMentah}
 				inventoriBahanMentah.itemKe[index].nama := bahanYangDibeli;
-				{membuat tanggal beli}
-				inventoriBahanMentah.itemKe[index].tanggalBeli.hari := dataSimulasi.itemKe[ID].tanggalSimulasi.hari;
-				inventoriBahanMentah.itemKe[index].tanggalBeli.bulan := dataSimulasi.itemKe[ID].tanggalSimulasi.bulan;
-				inventoriBahanMentah.itemKe[index].tanggalBeli.tahun := dataSimulasi.itemKe[ID].tanggalSimulasi.tahun;
 				
+				{membuat tanggal beli}
+				inventoriBahanMentah.itemKe[index].tanggalBeli := dataSimulasi.itemKe[ID].tanggalSimulasi;
+				
+				{membuat tanggal kadaluarsa}
+				inventoriBahanMentah.itemKe[index].tanggalKadaluarsa := inventoriBahanMentah.itemKe[index].tanggalBeli;
+				for i:= 1 to dataBahanMentah.itemKe[cariKadaluarsa(bahanYangDibeli, dataBahanMentah)].durasiKadaluarsa do 
+				begin
+					updateTanggal(inventoriBahanMentah.itemKe[index].tanggalKadaluarsa);
+				end;
+								
 				inventoriBahanMentah.itemKe[index].jumlahTersedia := kuantitas;
 				dataSimulasi.itemKe[ID].jumlahDuit:=dataSimulasi.itemKe[ID].jumlahDuit-dataBahanMentah.itemKe[i].hargaBeli*kuantitas;
 				inventoriBahanMentah.banyakItem:= inventoriBahanMentah.banyakItem+1;
@@ -69,4 +78,21 @@ implementation
 		dataSimulasi.itemKe[ID].totalPengeluaran:= dataBahanMentah.itemKe[i].hargaBeli*kuantitas;
 		dataSimulasi.itemKe[ID].jumlahEnergi := dataSimulasi.itemKe[ID].jumlahEnergi-1;
 	end;
+	
+	function cariKadaluarsa(bahanYangDibeli : string; dataBahanMentah : tabelBahanMentah):integer;
+	{Mencari index bahanYangDibeli pada array dataBahanMentah}
+	{KAMUS}
+	var	i : integer;
+	{ALGORITMA}
+	begin
+		for i:=1 to dataBahanMentah.banyakItem do
+		begin
+			if (dataBahanMentah.itemKe[i].nama = bahanYangDibeli) then
+			begin
+				cariKadaluarsa := i;
+				break;
+			end;
+		end;
+	end;
+	
 end.
