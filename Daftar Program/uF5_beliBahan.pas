0a6acd029d7f6a15c76f1b1f35ba5e2edd51dcdb
alimutaw12 <36516140+alimutaw12@users.noreply.github.com>
2018-04-19 19:55:24 +0700

    update beliBahan
    menyesuaikan untuk restock dan menambah validasi jika kapasitas inventori penuh

--- Daftar Program/uF5_beliBahan.pas
+++ Daftar Program/uF5_beliBahan.pas
@@ -4,7 +4,7 @@ interface
 
 uses uP1_tipeBentukan, uP3_Umum;
 
-	procedure mainBeliBahan(ID : integer; dataBahanMentah: tabelBahanMentah;var dataSimulasi: tabelSimulasi;//hapus parameter yang tidak perlu
+	procedure mainBeliBahan(bahanYangDibeli: string; kuantitas: integer; ID : integer; dataBahanMentah: tabelBahanMentah;var dataSimulasi: tabelSimulasi;//hapus parameter yang tidak perlu
 									var inventoriBahanMentah : tabelBahanMentah );
 	{ Menjalankan fungsi beliBahan }
 	function cariKadaluarsa(bahanYangDibeli : string; dataBahanMentah : tabelBahanMentah):integer;
@@ -13,22 +13,15 @@ uses uP1_tipeBentukan, uP3_Umum;
 
 implementation
 
-	procedure mainBeliBahan(ID : integer; dataBahanMentah: tabelBahanMentah;var dataSimulasi: tabelSimulasi;//hapus parameter yang tidak perlu
+	procedure mainBeliBahan(bahanYangDibeli: string; kuantitas: integer; ID : integer; dataBahanMentah: tabelBahanMentah;var dataSimulasi: tabelSimulasi;//hapus parameter yang tidak perlu
 									var inventoriBahanMentah : tabelBahanMentah
 									);
 	var
-		bahanYangDibeli: String;
-		kuantitas: integer;
 		i,j,index: integer;
 		ada: boolean;
 		modal : longint;
 	{ Menjalankan fungsi beliBahan }
 	begin
-		write('Nama bahan: ');
-		readln(bahanYangDibeli);
-		write('Kuantitas: ');
-		readln(kuantitas);
-		
 		i:=1;
 		ada:=false;
 		while((i<=dataBahanMentah.banyakItem) and (ada=false)) do
@@ -38,45 +31,50 @@ implementation
 			else
 				i:=i+1;
 		end;
-		
+		{mengecek apakah bahan yang dibeli ada tidak di supermarket}
 		if(ada = false) then
 			writeln('Bahan yang ingin Anda beli tidak dijual disupermarket')
 		else
 		begin
-		
-			modal := dataSimulasi.itemKe[ID].jumlahDuit;
+			if(inventoriBahanMentah.banyakItem+1 <= dataSimulasi.itemKe[ID].kapasitasMaxInventori) then
+			begin
+				modal := dataSimulasi.itemKe[ID].jumlahDuit;
 			
-			if ((modal>=dataBahanMentah.itemKe[i].hargaBeli*kuantitas) and (ada)) then
-			begin
-				writeln('Total harga: ', dataBahanMentah.itemKe[i].hargaBeli*kuantitas);
-				writeln('Pembelian Sukses');
-				index:= inventoriBahanMentah.banyakItem+1; 
+					if ((modal>=dataBahanMentah.itemKe[i].hargaBeli*kuantitas) and (ada)) then
+					begin
+						writeln('Total harga: ', dataBahanMentah.itemKe[i].hargaBeli*kuantitas);
+						writeln('Pembelian Sukses');
+						index:= inventoriBahanMentah.banyakItem+1; 
+						
+						{memasukan ke array inventoriBahanMentah}
+						inventoriBahanMentah.itemKe[index].nama := bahanYangDibeli;
+						
+						{membuat tanggal beli}
+						inventoriBahanMentah.itemKe[index].tanggalBeli := dataSimulasi.itemKe[ID].tanggalSimulasi;
+						
+						{membuat tanggal kadaluarsa}
+						inventoriBahanMentah.itemKe[index].tanggalKadaluarsa := inventoriBahanMentah.itemKe[index].tanggalBeli;
+						for j:= 1 to dataBahanMentah.itemKe[cariKadaluarsa(bahanYangDibeli, dataBahanMentah)].durasiKadaluarsa do 
+						begin
+							updateTanggal(inventoriBahanMentah.itemKe[index].tanggalKadaluarsa);
+						end;
+										
+						inventoriBahanMentah.itemKe[index].jumlahTersedia := kuantitas;
+						dataSimulasi.itemKe[ID].jumlahDuit:=dataSimulasi.itemKe[ID].jumlahDuit-dataBahanMentah.itemKe[i].hargaBeli*kuantitas;
+						inventoriBahanMentah.banyakItem:= inventoriBahanMentah.banyakItem+1;
+						dataSimulasi.itemKe[ID].totalPengeluaran:= dataSimulasi.itemKe[ID].totalPengeluaran+ dataBahanMentah.itemKe[i].hargaBeli*kuantitas;
+						dataSimulasi.itemKe[ID].jumlahEnergi := dataSimulasi.itemKe[ID].jumlahEnergi-1;
+					end else
+					begin
+						writeln('Total harga: ', dataBahanMentah.itemKe[i].hargaBeli*kuantitas);
+						writeln('Pembelian Gagal');
+					end;
 				
-				{memasukan ke array inventoriBahanMentah}
-				inventoriBahanMentah.itemKe[index].nama := bahanYangDibeli;
-				
-				{membuat tanggal beli}
-				inventoriBahanMentah.itemKe[index].tanggalBeli := dataSimulasi.itemKe[ID].tanggalSimulasi;
-				
-				{membuat tanggal kadaluarsa}
-				inventoriBahanMentah.itemKe[index].tanggalKadaluarsa := inventoriBahanMentah.itemKe[index].tanggalBeli;
-				for j:= 1 to dataBahanMentah.itemKe[cariKadaluarsa(bahanYangDibeli, dataBahanMentah)].durasiKadaluarsa do 
-				begin
-					updateTanggal(inventoriBahanMentah.itemKe[index].tanggalKadaluarsa);
-				end;
-								
-				inventoriBahanMentah.itemKe[index].jumlahTersedia := kuantitas;
-				dataSimulasi.itemKe[ID].jumlahDuit:=dataSimulasi.itemKe[ID].jumlahDuit-dataBahanMentah.itemKe[i].hargaBeli*kuantitas;
-				inventoriBahanMentah.banyakItem:= inventoriBahanMentah.banyakItem+1;
+
 			end else
-			begin
-				writeln('Total harga: ', dataBahanMentah.itemKe[i].hargaBeli*kuantitas);
-				writeln('Pembelian Gagal');
-			end;
+				writeln('kapasitas inventori tidak mencukupi');
 		end;
-		
-		dataSimulasi.itemKe[ID].totalPengeluaran:= dataBahanMentah.itemKe[i].hargaBeli*kuantitas;
-		dataSimulasi.itemKe[ID].jumlahEnergi := dataSimulasi.itemKe[ID].jumlahEnergi-1;
+			
 	end;
 	
 	function cariKadaluarsa(bahanYangDibeli : string; dataBahanMentah : tabelBahanMentah):integer;
