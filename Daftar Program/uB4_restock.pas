1d08bbe799e4646c208d1c6e3531728fd9b7f2b8
alimutaw12 <36516140+alimutaw12@users.noreply.github.com>
2018-04-19 19:53:22 +0700

    update uB4_restock

--- Daftar Program/uB4_restock.pas
+++ Daftar Program/uB4_restock.pas
@@ -36,20 +36,35 @@ implementation
 	if(inventoriBahanMentah.banyakItem+dataBahanMentah.banyakItem <= dataSimulasi.itemKe[ID].kapasitasMaxInventori) then
 	begin
 		{mengecek apakah uang saat ini cukup atau tidak}
-		if(dataSimulasi.item[ID].JumlahDuit>=totalHarga) then
+		if(dataSimulasi.itemKe[ID].JumlahDuit>=totalHarga) then
 		begin
+			writeln('Restock bahan mentah');
 			for i:=1 to dataBahanMentah.banyakItem do
 			begin
+				write('Pembelian ', dataBahanMentah.itemKe[i].nama);
 				if((dataBahanMentah.itemKe[i].hargaBeli>0) and (dataBahanMentah.itemKe[i].hargaBeli<=500)) then
-					mainBeliBahan(dataBahanMentah.itemKe[i].nama, 20, ID, dataBahanMentah, dataSimulasi, inventoriBahanMentah)
+					begin
+					writeln(' sebanyak 20');
+					mainBeliBahan(dataBahanMentah.itemKe[i].nama, 20, ID, dataBahanMentah, dataSimulasi, inventoriBahanMentah);
+					end
 				else if((dataBahanMentah.itemKe[i].hargaBeli>500) and (dataBahanMentah.itemKe[i].hargaBeli<=2000)) then
-					mainBeliBahan(dataBahanMentah.itemKe[i].nama, 10, ID, dataBahanMentah, dataSimulasi, inventoriBahanMentah)
+					begin
+					writeln(' sebanyak 10');
+					mainBeliBahan(dataBahanMentah.itemKe[i].nama, 10, ID, dataBahanMentah, dataSimulasi, inventoriBahanMentah);
+					end
 				else if((dataBahanMentah.itemKe[i].hargaBeli>500) and (dataBahanMentah.itemKe[i].hargaBeli<=2000)) then
-					mainBeliBahan(dataBahanMentah.itemKe[i].nama, 5, ID, dataBahanMentah, dataSimulasi, inventoriBahanMentah)
+					begin
+					writeln(' sebanyak 5');
+					mainBeliBahan(dataBahanMentah.itemKe[i].nama, 5, ID, dataBahanMentah, dataSimulasi, inventoriBahanMentah);
+					end
 				{dataBahanMentah.itemKe[i].hargaBeli>5000}
 				else
+					begin
+					writeln(' sebanyak 1');
 					mainBeliBahan(dataBahanMentah.itemKe[i].nama, 1, ID, dataBahanMentah, dataSimulasi, inventoriBahanMentah);
+					end;
 			end;
+			writeln('total pengeluaran restock kali ini adalah ',totalHarga);
 		{uang sekarang tidak cukup}
 		end else
 			writeln('Uang tidak cukup, tidak bisa melakukan restock');
